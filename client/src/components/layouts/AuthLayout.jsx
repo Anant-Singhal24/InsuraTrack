34ae006ddd629bc1af2701c520893a9ef4901aa8
Anant Singhal <anantsinghal0124@gmail.com>
2025-06-11 17:46:12 +0530

    fix login page

--- client/src/components/layouts/AuthLayout.jsx
+++ client/src/components/layouts/AuthLayout.jsx
@@ -17,7 +17,7 @@ const AuthLayout = () => {
   }
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50 dark:bg-gray-900 py-12 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen flex items-center justify-center bg-blue-50/50 dark:bg-gray-900 py-12 px-4 sm:px-6 lg:px-8">
       <div className="absolute top-4 right-4">
         <ThemeToggle />
       </div>
